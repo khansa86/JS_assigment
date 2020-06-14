88c191b3547b21b023d217c8f51e1e4f9b5f9dca
khansa86 <khansaasif64@gmail.com>
2020-06-14 17:22:58 +0500

    on_the_way

---
+++ app.js
@@ -0,0 +1,308 @@
+// // assignmen#1
+// // task1
+// alert("Hello user");
+// // //task no2
+//  alert("Error! Please enter a valid password");
+// // //task no 3
+// alert("Welcome to JS land...."+"\r\n"+"Happy coding"); 
+
+// // //task4
+// alert("Welcome to JS land");
+// confirm("prevent this page from creating")    
+// // task 5 in console redo
+// // 6 and 7 done 
+
+
+
+
+// asignment#2
+// task1
+// var UserName;
+// // task2
+// var MyName=" Khasna Asif";
+// // task3
+// var message="Hello World";
+// alert (message);
+// // task4
+// var name= prompt("what is you name: ");
+// var age= prompt ("how old are you");
+// var course =prompt("enter your course name");
+// alert("your name "+name);
+// alert("your age"+age);
+// alert("your course"+course);
+// task5 
+// alert("Pizza"+"\n"+"Pizz"+"\n"+"piz"+"\n"+"pi"+"\n"+"p");
+// //task6
+// var email="example@gmail.com"
+// alert("my email address is"+ email);
+// //task7
+// var book="a smarter way to learn javascript"
+// alert ("I am trying to learn the book "+book);
+// task8
+// document.write("Yeah! i can write html content through javascript");
+// // task9
+// alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+
+
+
+//assignment#3 
+// // task1
+// var age ="I am 21 years old"
+// alert(age);
+// task2
+// var visit=" ";
+// alert("you have visited this page "+visit +"times"); //redo how to keep track of visit
+// task3
+// var BirthYear=(2000);
+// document.write("My bithyear is "+BirthYear+ "<br>"+ "The type of my birth-year is number");  
+// task4
+// var visitorName=prompt("your name");
+// var ProductTitle=prompt("What have you ordered?");
+// var Quantity=prompt("how many(quantity)?");
+// document.write(visitorName + " ordered "+Quantity+ " " +ProductTitle+"(s)");
+
+
+// // assignment#4
+// // task1
+// var a="honey",c="apple",b="cake";
+// document.write(a+" "+b+" "+c)
+// // task2
+// var a=b=c=d=1099;
+// var 1a=1b=1c=1099;
+// var ?21a=?1a=?1j=?1=10999;
+// task3
+// var a="Rules for naming JS variables";
+// var b1= "numbers",b2="$",b3="_";
+// var c1= "letters",c2="$",c3="+";
+// var d=" case sensitive";
+// var e="keywords"
+// document.write(a);
+// document.write("b) Variable names can only contain "+b1+", "+b2+","+" and "+b3+"<br>");
+// document.write("c) Variable names must begin with "+c1+", "+c2+","+" or "+c3+"<br>");
+// document.write("d) Variable names are "+d+"<br>");
+// document.write(" e) Variable names should not be JS  "+e+"<br>");                       
+
+// assignment#5
+// // task1
+// var a=5;
+// var b=3;
+// var c= a+b;
+// alert("the sum of "+a+" and "+b+" is "+c);
+// // task2
+// var a=5;
+// var b=3;
+// var c= a-b;
+// alert("the subtraction of "+a+" and "+b+" is "+c);
+// var a=5;
+// var b=3;
+// var c= a*b;
+// alert("the product of "+a+" and "+b+" is "+c);
+// var a=5;
+// var b=3;
+// var c= a/b;
+// alert("the division of "+a+" by "+b+" is "+c);
+// var a=5;
+// var b=3;
+// var c= a%b;
+// alert("the remainder of "+a+" divided by "+b+" is "+c);
+// task3
+// var num;
+// document.write("The value of decleared variale is "+num);
+// num=5;
+// document.write(" The initial value is  "+num);
+// num++
+// document.write(" The value after increment is "+num);
+// num=num+7;
+// document.write(" The value after addition is "+ num );
+// num--
+// document.write(" The value after decrement is "+num);
+// num=num%3
+// document.write(" The output remainder is "+num)
+// // Task4
+// var costOf1Ticket=600;
+// var noOfTickets=prompt(" How many tickets you want to buy? ");
+// var totalCost=costOf1Ticket*noOfTickets;
+// document.write(" Total cost to buy "+noOfTickets+" tickets is "+totalCost);
+// task5
+// var i=4
+//     for (var j=1; j<11; j++){
+//         document.write(i+" x "+j+" = "+j*i+"<br>")
+
+
+//     }                                                   
+// // task6
+// var celcius=prompt(" Enter temp in celcius");
+// var celciusToFahrenheit=(celcius*(9/5))+32;
+// document.write(celcius+" oC is "+ celciusToFahrenheit+" oF"+"<br>");
+// var fahrenheit=prompt(" Enter temp in fahrenheit");
+// var fahrenheitTocelcius=(fahrenheit-32)*(5/9);
+// document.write(fahrenheit+" oF is "+fahrenheitTocelcius+" oC"+"<br>");           
+// //task7
+// var item1Price = 400;
+// var item2Price = 500;
+// var quantityOfItem1 = prompt("how many item1 u bought: ");
+// var quantityOfItem2 = prompt("how many item2 u bought: ");
+// var item1bought = item1Price * quantityOfItem1;
+// var item2bought = item2Price * quantityOfItem2;
+// var deliveryCharges = 100;
+// var totalCost = deliveryCharges + item1bought + item2bought;
+// document.write(
+//     "The price of item 1 is "+ item1Price+"<br>"+"The quantity of item 1 bought is "+ quantityOfItem1+"<br>"
+//     + "The price of item 2 is "+ item2Price+"<br>"+"The quantity of item 1 bought is "+ quantityOfItem2+"<br>"
+//     +"The shipping charges are "+deliveryCharges+"<br>"+
+//     "<br>"+"Total cost of your order is "+totalCost
+// );
+// task8
+// var marksObtained = prompt("enter marks you obtained", "your marks out of 1000");
+// var totalMarks = 1000;
+// var percentage = (marksObtained * 100) / totalMarks;
+// document.write("The total marks are " + totalMarks + "<br>" + "Marks obtained by student " + marksObtained + "<br>"
+//  + "The percentage of student is "+percentage);
+//// task9
+// var rateOfsaudiRiyal=28;
+// var rateOfUSD=104.80;
+// var riyalIHave=25;
+// var dollarIHave=10;
+// var moneyIHAveInPkr=(rateOfsaudiRiyal*riyalIHave)+(dollarIHave*rateOfUSD);
+// document.write("Money in PKR "+moneyIHAveInPkr);
+// task10
+// var a= 10;
+// var b=((a+2)*10)/2;
+// document.write(b)
+// // task11
+// var currentYear=2020;
+// var yourBirthYear=prompt("Type your Birthyear",2020);
+// var age=currentYear-yourBirthYear;
+// document.write("your age is "+age);
+// // task12
+// var radius = prompt("type radius of the circle in m ", 10);
+// var pi = 3.142;
+// var circumference = (2 * pi) * radius;
+// var area = pi*(radius*radius);
+//     document.write("radius "+radius+"<br>"+"circumference: " + circumference + "<br>" +
+//     "area: "+area);
+// // task13
+// var favoriteSnack=prompt("type your fav snack","choclate");
+// var snackPerDay=prompt("How many of"+favoriteSnack+"you eat each day",2);
+// var ageNow=prompt("How old are you",18);
+// var supplyTime=prompt("To what age would you like to eat "+favoriteSnack);
+// var totalSnackNeeded= ((supplyTime-ageNow)*365)*snackPerDay;
+// document.write("You will need "+totalSnackNeeded+" packets of "+favoriteSnack+" to eat "+ snackPerDay +
+//  " per day "+" till age "+ supplyTime+" from age "+ageNow);
+
+
+// assignment#6-9
+// // task1
+// var a=10;
+// document.write("The value of a is"+ a +"<br>");
+// document.write("The value of ++a is"+ ++a +"<br>");
+// document.write("Now the value of a is "+ a +"<br>");
+// document.write("The value of a++ is"+ a++ +"<br>");
+// document.write("Now the value of a is "+ a +"<br>");
+// document.write("The value of --a is"+ --a +"<br>");
+// document.write("Now the value of a is "+ a +"<br>");
+// document.write("The value of a-- is"+ a-- +"<br>");
+// document.write("Now the value of a is "+ a +"<br>");
+//// task2
+// var a = 2, b = 1;
+// var result = --a - --b +  ++b + b--;
+// //            ^1 -  ^0 +   ^1 +^1-0 =3 //result
+// document.write(result)
+// // task3
+// userName=prompt("What is your name");
+// alert("Hello "+userName);
+// task5
+// numberForTable=prompt("Type the number you want table ",5);
+// var i=numberForTable
+//      for (var j=1; j<11; j++){
+//          document.write(i+" x "+j+" = "+j*i+"<br>")
+
+
+//     } 
+// task 6
+// var subject1=prompt("Type name of 1st Subject: ");
+// var subject2=prompt("Type name of 2nd Subject: ");
+// var subject3=prompt("Type name of 3rd Subject: ");
+// var obtainedMarks1=prompt("Type marks obtained in "+subject1);
+// var obtainedMarks2=prompt("Type marks obtained in "+subject2);
+// var obtainedMarks3=prompt("Type marks obtained in "+subject3);
+// var totalMarks1subject=100;
+// var totalMarks=100;
+// var percentage=((obtainedMarks1+obtainedMarks2+obtainedMarks3)*100)/totalMarks ;
+
+
+
+
+// // assignment#9-11
+// task1
+// var city="karachi"
+// var userCity= prompt("Type your city name ","karachi" );
+// userCity=userCity.toLowerCase();
+// if (userCity===city){
+//     document.write("Welcome to the city of lights")
+// }
+// else{
+//     ("You may leave")
+// }
+// task2
+// var gender=prompt("Type your gender","gender");
+// gender=gender.toLowerCase();
+// var male=("Hello Sir");
+// var female=("Hello Madam");
+// if (gender==="male"){
+//     alert(male);
+// }
+// else if (gender==="female"){
+//     alert(female);
+// }
+// else{
+//     alert("Hi there");
+// }
+// // // task3
+// var trafficLight=prompt("Type color of Traffic light","red");
+// trafficLight=trafficLight.toLowerCase();
+// var red=("Must Stop");
+// var green=("Move Now");
+// var yellow=("Ready to Move");
+// if (trafficLight==="red"){
+//     alert(red);
+// }
+// else if (trafficLight==="green"){
+//     alert(green);
+// }
+// else if (trafficLight==="yellow"){
+//     alert(yellow);
+// }
+// else{
+//     alert("Invalid");
+// }
+// task4
+var fuelLeft=prompt("How much fuel is left ",0.25+"l");
+if (fuelLeft>0.25+"l"){
+    alert("fuel is good");
+}
+else if (fuelLeft<=0.25+"l"){
+    alert("low fuel remaining please refill");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
